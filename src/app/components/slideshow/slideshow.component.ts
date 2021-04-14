5b407834f2a4bb1257c91c316646d88887ba5bd4
Janio Isacura <janioisacura@gmail.com>
2021-04-14 15:16:27 -0300

    Implementación del swiper en slideshow component

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,19 +1,37 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/cartelera-response';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.scss']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit {
 
   @Input() movies: Movie[];
+
+  public swiper: Swiper;
 
   constructor() { }
 
   ngOnInit(): void {
     console.log('Estas son las películas que me llegan mediante el decorador @Input', this.movies)
   }
+
+  ngAfterViewInit(): void {
+    this.swiper = new Swiper('.swiper-container', {
+      loop: true,
+    });
+  }
+
+  onSlidePrev(){
+    this.swiper.slidePrev();
+  }
+
+  onSlideNext(){
+    this.swiper.slideNext();
+  }
+
 
 }
